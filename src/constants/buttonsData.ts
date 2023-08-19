97a187cae205045e869002ed5aa0b0798cb87ce0
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2023-08-19 18:34:28 +0200

    add buttons positions

--- src/constants/buttonsData.ts
+++ src/constants/buttonsData.ts
@@ -6,24 +6,24 @@ import single from "../../public/singleMuro.jpg";
 export const BUTTONS_DATA: IButtonData[] = [
   {
     title: "Fourway",
-    description:
-      "Tiene forma de cruz y podemos representar diferentes outfits tanto camisetas con pantalones como tops deportivos y shorts en el caso de WMNS",
+    description: "Descripción del mueble y de como representar SKUs...",
     image: "/../public/WhatsApp Image 2023-06-01 at 23.03.56.jpeg",
     position: { top: 230, left: 520 },
     class: "buttonClassFW",
   },
   {
     title: "Workhorst",
-    description: "outfit 1 del Workhorst",
+    description:
+      "Hablar de producto, dbar, face-out, high capacity, low capacity",
     image: "/../public/workhorst.jpg",
-    position: { top: 50, left: 100 },
+    position: { top: 400, left: 520 },
     class: "buttonClassWH",
   },
   {
     title: "Single",
-    description: "Outfit 2 del Single",
+    description: "Maximo 4 SKUs, hablar del máximo de units por modelo...",
     image: "/../public/singleMuro2.jpg",
-    position: { top: 20, left: 20 },
+    position: { top: 270, left: 240 },
     class: "buttonClassSing",
   },
 ];
