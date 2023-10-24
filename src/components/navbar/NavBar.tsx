f90ebd0ab8abdb8070ffc18913854980ab7c6594
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2023-10-25 00:43:25 +0200

    houston tenemos un problema

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./NavBar.module.css";
+import styles from "./Navbar.module.css";
 import nikeLogo from "../../../public/swooshLogo.png";
 
 export const NavBar = () => {
